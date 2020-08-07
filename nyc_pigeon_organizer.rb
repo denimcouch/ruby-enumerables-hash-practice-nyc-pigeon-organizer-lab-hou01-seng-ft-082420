8afe0180b3a0e182c4f9a1ae1eef52d7cf0c153f
Alex Mata <alexmata92@ymail.com>
2020-08-07 17:30:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,20 +1,8 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key, value), new_array|
-    #binding.pry
-    value.each do |inner_key, names|
-      #binding.pry
-      names.each do |name|
-        #binding.pry
-        if new_array[name] == nil
-          new_array[name] = {}
-        end
-        binding.pry 
-      end 
-    end 
-    new_array
-  end 
+  
+  
   binding.pry 
-end
+end 
 
